9ff6e38871e2d4f48017e5017302ce4535d5742f
Nelson Chen <crazysim@gmail.com>
2015-01-06 01:23:42 -0800

    Ported Caret color

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,10 +1,15 @@
-function __caret
-
+function __caret_color
+	if [ $USER = "root" ]
+	  echo -n "red"
+	else
+	  echo -n "magenta"
+	end
 end
 
+
 function fish_prompt
-	echo -s (set_color -o cyan)(whoami)@(hostname)
-	echo '>'
+	echo -s (set_color -o cyan)(whoami)(set_color normal)@(hostname| cut -d . -f 1)
+	echo -s (set_color (__caret_color))'# '(set_color normal)
 end
 
 function fish_right_prompt -d "Write out the right prompt"
